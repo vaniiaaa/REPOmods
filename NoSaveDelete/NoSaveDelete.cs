58406bef49ef5168b7900e400159d7f1059d61f9
Ivan Zhavner <zhavnervan@gmail.com>
2025-11-18 18:42:08 +0300

    blocks in extra functions of arena

--- NoSaveDelete/NoSaveDelete.cs
+++ NoSaveDelete/NoSaveDelete.cs
@@ -7,23 +7,23 @@ namespace NoSaveDelete
     [BepInPlugin("com.vaniiaaa.nosavedelete", "NoSaveDelete", "1.0.0")]
     public class NoSaveDeletePlugin : BaseUnityPlugin
     {
-        internal static ManualLogSource Log;
+        private static ManualLogSource logger;
 
         private void Awake()
         {
-            Log = Logger;
-            
+            logger = Logger;
             var harmony = new Harmony("com.vaniiaaa.nosavedelete");
-            
+            harmony.PatchAll();
+            logger.LogInfo("NoSaveDelete is loaded");
         }
+
 
         [HarmonyPatch(typeof(DataDirector), "SaveDeleteCheck")]
         public class DataDirectorSaveDeleteCheckPatch
         {
             static bool Prefix(bool _leaveGame)
             {
-            
-                
+                logger.LogInfo("SaveDeleteCheck blocked");
                 return false;
             }
         }
@@ -33,9 +33,33 @@ namespace NoSaveDelete
         {
             static bool Prefix(string saveFileName)
             {
-                
-                
+                logger.LogInfo("SaveFileDelete blocked");
                 return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(RunManager), "ResetProgress")]
+        public class ResetProgressPatch
+        {
+            static bool Prefix()
+            {
+                logger.LogInfo("ResetProgress blocked - progress preserved");
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(RunManager), "ChangeLevel")]
+        public class ChangeLevelPatch
+        {
+            static void Prefix(ref bool completedLevel, ref bool levelFailed)
+            {
+                
+                if (levelFailed && RunManager.instance.levelCurrent == RunManager.instance.levelArena)
+                {
+                    completedLevel = false;
+                    levelFailed = false; 
+                    logger.LogInfo("ChangeLevel after Arena - levelFailed flag reset to prevent progress loss");
+                }
             }
         }
     }
